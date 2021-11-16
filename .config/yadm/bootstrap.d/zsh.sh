ced28b4460fd27b4859adda533bae808f8474c2e
bebyx <bebyx@protonmail.com>
2021-11-16 18:54:47 +0200

    Download custom Bash improved theme

--- .config/yadm/bootstrap.d/zsh.sh
+++ .config/yadm/bootstrap.d/zsh.sh
@@ -6,4 +6,5 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
 	echo "Oh-My-Zsh is installed on this userspace"
 else
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	curl -fsSLo "$ZSH/themes/bebyx.zsh-theme" https://gist.githubusercontent.com/bebyx/38ce753760f4f3a71e56dc081e64aa8e/raw/97c7395ef610afafc0d971c3090252264a37e445/bebyx.zsh-theme
 fi
