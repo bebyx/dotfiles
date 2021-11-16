cd60ee8a24d3a3bebbabc735824d5f5210b09a29
bebyx <bebyx@protonmail.com>
2021-11-16 23:29:29 +0200

    Update OMZ install command for batch mode

--- .config/yadm/bootstrap.d/zsh.sh
+++ .config/yadm/bootstrap.d/zsh.sh
@@ -5,6 +5,7 @@
 if [ -d "$HOME/.oh-my-zsh" ]; then
 	echo "Oh-My-Zsh is installed on this userspace"
 else
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	echo "Installing Oh-My-Zsh"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 	curl -fsSLo "$ZSH/themes/bebyx.zsh-theme" https://gist.githubusercontent.com/bebyx/38ce753760f4f3a71e56dc081e64aa8e/raw/97c7395ef610afafc0d971c3090252264a37e445/bebyx.zsh-theme
 fi
