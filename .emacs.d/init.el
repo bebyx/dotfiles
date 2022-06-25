41fdee1f4e94fe4a9ba2c7d9f907b6229ef79a67
bebyx <artem@bebyx.co.ua>
2022-06-25 14:48:26 +0300

    Add basic init.el config

---
+++ .emacs.d/init.el
@@ -0,0 +1,11 @@
+(setq visible-bell t) ; switch off annoying bell sound, instead bell is visible
+(load-theme 'wombat) ; dark theme
+(set-face-attribute 'default nil :family "Source Code Pro" :foundry "ADBO" :slant 'normal :weight 'normal :height 140 :width 'normal)
+
+(global-linum-mode t)
+(fringe-mode 15)
+
+(column-number-mode t)
+(size-indication-mode t)
+(scroll-bar-mode -1)
+
