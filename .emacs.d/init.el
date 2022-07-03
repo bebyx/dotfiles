675aab100793340da1f753f04ed9308c404a4c00
bebyx <artem@bebyx.co.ua>
2022-07-03 14:53:21 +0300

    Initialize packages
    1. Add config for using Emacs packages.
    2. Add reverse-im.el package to make Emacs keybindings work with Ukrainian layout.
    3. Add markdown-mode package.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,3 +9,30 @@
 (size-indication-mode t)
 (scroll-bar-mode -1)
 
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize) ; activate all the packages
+
+;; Fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package reverse-im
+  :custom
+  (reverse-im-input-methods '("ukrainian-computer"))
+  :config
+  (reverse-im-mode t))
+
+(use-package markdown-mode)
