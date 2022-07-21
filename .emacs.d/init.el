c40cff409af457eb7d77ed74f5fa3eb16a779ae4
bebyx <artem@bebyx.co.ua>
2022-07-21 16:24:50 +0300

    Add helm, IDE packages and other tweaks
    Configure navigation with Helm.
    Configure Java IDE (LSP, Treemacs, etc.).
    Minor tweaks related to line numbering, auto-update of packages, and other.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,12 +2,23 @@
 (load-theme 'wombat) ; dark theme
 (set-face-attribute 'default nil :family "Source Code Pro" :foundry "ADBO" :slant 'normal :weight 'normal :height 140 :width 'normal)
 
-(global-linum-mode t)
-(fringe-mode 15)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(fringe-mode 10)
 
-(column-number-mode t)
 (size-indication-mode t)
 (scroll-bar-mode -1)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Initialize package sources
 (require 'package)
@@ -29,10 +40,39 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "00:00"))
+
+;; Use keybindings when Ukrainian layout is on
 (use-package reverse-im
   :custom
   (reverse-im-input-methods '("ukrainian-computer"))
   :config
   (reverse-im-mode t))
 
+;; Helm config
+(use-package helm
+  :bind (
+	 ("M-x" . helm-M-x)
+	 ("C-x C-f" . helm-find-files)
+	 ("C-s" . helm-occur))
+  :config
+  (helm-mode 1))
+
+;; Dev, mostly Java, config
 (use-package markdown-mode)
+(use-package lsp-java
+  :config
+  (add-hook 'java-mode-hook 'lsp))
+(use-package gradle-mode)
+(use-package helm-lsp)
+(use-package lsp-treemacs)
+(use-package treemacs
+  :bind (
+	 ("C-t" . treemacs)))
